52ab16f89412725bc06307f242472e63e6c78dcc
Fujisawa-dx <95336208+Fujisawa-dx@users.noreply.github.com>
2023-11-17 13:54:33 +0900

    Add files via upload

--- event.js
+++ event.js
@@ -1,40 +1,52 @@
 function next (idname){
     document.getElementById(idname).style.display = "block";
-    scrollBy(0,100)
+    window.scroll(0,document.documentElement.scrollHeight-document.documentElement.clientHeight)
 }
 
 function procVisible(){
-    var ff = 1;
+    var ff = 0;
 
     if (document.getElementById("food1").checked == true){
-         ff = ff + 1
-    }else{};
+         ff = ff + 2
+    }else{ff= ff + 1};
 
     if (document.getElementById("fire1").checked == true){
-        ff = ff + 20
+          ff = ff + 20
     }else{ff = ff + 10};
 
-    if (document.getElementById("place1").checked == true){
-        ff = ff + 200
-    }else{ff=ff+100};
+    if (document.getElementById("fire-works1").checked == true){
+          ff = ff + 200
+    }else{ff = ff + 100};
 
-    if (document.getElementById("place2").checked == true){
-        ff = ff + 2000;
-    }else{ff= ff + 1000};
+    if (document.getElementById("fire-miss1").checked == true){
+          ff = ff + 2000
+    }else{ff = ff + 1000};
 
-    if (document.getElementById("place3").checked == true){
-        ff = ff + 20000;
+    if (document.getElementById("fire-event1").checked == true){
+          ff = ff + 20000
     }else{ff = ff + 10000};
 
+    if (document.getElementById("place1").checked == true){
+          ff = ff + 200000
+    }else{ff = ff + 100000};
+
+    if (document.getElementById("place2").checked == true){
+          ff = ff + 20000000;
+    }else{ff = ff + 10000000};
+
+    if (document.getElementById("place3").checked == true){
+          ff = ff + 200000000;
+    }else{ff = ff + 100000000};
+
+    if (document.getElementById("place5").checked == true){
+          ff = ff + 2000000000;
+    }else{ff = ff + 1000000000};
+
     if (document.getElementById("cheer1").checked == true){
-        ff = ff + 200000
-    }else{ff= ff + 100000};
+          ff = ff + 20000000000;
+    }else{ff = ff + 10000000000};
 
     strff = String(ff)
     url = location.href
     window.location = "result.html?check=" + strff
-}
-
-function test(){
-    document.getElementById("eventProcedure").style.display = "block";
 }
--- result.js
+++ result.js
@@ -2,25 +2,41 @@ var pflag = location.search;
 var ff = 1;
 
 if (pflag.charAt(pflag.length -1) == 2){
-    document.getElementById("eventProcedure").style.display = "block";
+    document.getElementById("food").style.display = "block";
 }else{};
 
 if (pflag.charAt(pflag.length -2) ==2){
-    document.getElementById("fireProcedure").style.display = "block";
+    document.getElementById("fire").style.display = "block";
 }else{};
 
 if (pflag.charAt(pflag.length -3) ==2){
-    document.getElementById("dorosenyo").style.display = "block";
+    document.getElementById("fire-works").style.display = "block";
 }else{};
 
 if (pflag.charAt(pflag.length -4) ==2){
-    document.getElementById("parksenyo").style.display = "block";
+    document.getElementById("fire-miss").style.display = "block";
 }else{};
 
 if (pflag.charAt(pflag.length -5) ==2){
-    document.getElementById("koukyosiyo").style.display = "block";
+    document.getElementById("fire-event").style.display = "block";
 }else{};
 
 if (pflag.charAt(pflag.length -6) ==2){
-    document.getElementById("cheering").style.display = "block";
+    document.getElementById("place1").style.display = "block";
+}else{};
+
+if (pflag.charAt(pflag.length -7) ==2){
+    document.getElementById("place2").style.display = "block";
+}else{};
+
+if (pflag.charAt(pflag.length -8) ==2){
+    document.getElementById("place3").style.display = "block";
+}else{};
+
+if (pflag.charAt(pflag.length -10) ==2){
+    document.getElementById("place5").style.display = "block";
+}else{};
+
+if (pflag.charAt(pflag.length -11) ==2){
+    document.getElementById("cheer").style.display = "block";
 }else{};
